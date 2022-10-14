922373a6eeb87800d22d8758139bfef587e0876e
melanierevolorio <gabrielanij@gmail.com>
2022-10-14 02:37:41 -0600

    Modified theme

--- AngularIS/src/SuperGasApp/ClientApp/src/app/navbar/navbar.component.ts
+++ AngularIS/src/SuperGasApp/ClientApp/src/app/navbar/navbar.component.ts
@@ -1,7 +1,29 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
+//import { Component, OnInit, AfterViewInit } from '@angular/core';
+//import { User } from '../../models/user.model';
+//import { AuthService } from '../../services/authentication.service';
+//import { Observable} from 'rxjs';
+
+//@Component({
+//  selector: 'navbar-component',
+//  templateUrl: './navbar.component.html',
+//  styleUrls: ['./navbar.component.scss']
+//})
+//export class NavbarComponent implements OnInit {
+//  user$: Observable<User> = new Observable<User>();
+
+//  constructor(private auth: AuthService) {
+//  }
+
+//  ngOnInit(): void {
+//    this.user$ = this.auth.getUser();
+//  }
+//}
+
+
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { User } from '../../models/user.model';
 import { AuthService } from '../../services/authentication.service';
-import { Observable} from 'rxjs';
 
 @Component({
   selector: 'navbar-component',
@@ -10,11 +32,26 @@ import { Observable} from 'rxjs';
 })
 export class NavbarComponent implements OnInit {
   user$: Observable<User> = new Observable<User>();
-
-  constructor(private auth: AuthService) {
+  sidenavWidth = 4;
+  ngStyle: string;
+    constructor(private auth: AuthService) {
   }
 
   ngOnInit(): void {
     this.user$ = this.auth.getUser();
   }
+
+  increase() {
+    this.sidenavWidth = 15;
+    console.log('increase sidenav width');
+  }
+  decrease() {
+    this.sidenavWidth = 4;
+    console.log('decrease sidenav width');
+  }
+  // sidenavToggle() {
+  //   this.ngStyle = 'this.sidenavWidth = 15';
+  //   console.log('sidenav width incrases');
+  // }
+
 }
